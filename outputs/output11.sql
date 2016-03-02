dcb7a432a015221d80e1499543f1f4a0edd30b93
Jesus <chuy8jay@gmail.com>
2016-03-01 17:59:14 -0700

    Finished q11, q14 removed q14.txt 16 and 8

---
+++ outputs/output11.sql
@@ -0,0 +1,56 @@
++-----+------------------------+
+| sid | dname                  |
++-----+------------------------+
+|   1 | Mathematics            |
+|   3 | Mathematics            |
+|   4 | Sanitary Engineering   |
+|  26 | Chemical Engineering   |
+|  27 | Chemical Engineering   |
+|  28 | Chemical Engineering   |
+|  29 | Chemical Engineering   |
+|  29 | Chemical Engineering   |
+|  29 | Chemical Engineering   |
+|  32 | Chemical Engineering   |
+|  33 | Chemical Engineering   |
+|  34 | Chemical Engineering   |
+|  35 | Chemical Engineering   |
+|  36 | Civil Engineering      |
+|  37 | Civil Engineering      |
+|  40 | Civil Engineering      |
+|  45 | Civil Engineering      |
+|  47 | Civil Engineering      |
+|  47 | Civil Engineering      |
+|  48 | Civil Engineering      |
+|  49 | Civil Engineering      |
+|  49 | Civil Engineering      |
+|  50 | Civil Engineering      |
+|  53 | Civil Engineering      |
+|  54 | Civil Engineering      |
+|  55 | Civil Engineering      |
+|  56 | Civil Engineering      |
+|  58 | Civil Engineering      |
+|  59 | Civil Engineering      |
+|  60 | Civil Engineering      |
+|  61 | Civil Engineering      |
+|  64 | Industrial Engineering |
+|  66 | Industrial Engineering |
+|  66 | Industrial Engineering |
+|  70 | Industrial Engineering |
+|  73 | Industrial Engineering |
+|  74 | Industrial Engineering |
+|  76 | Industrial Engineering |
+|  78 | Industrial Engineering |
+|  79 | Industrial Engineering |
+|  81 | Industrial Engineering |
+|  85 | Mathematics            |
+|  88 | Mathematics            |
+|  90 | Mathematics            |
+|  91 | Mathematics            |
+|  93 | Mathematics            |
+|  94 | Mathematics            |
+|  96 | Mathematics            |
+|  96 | Mathematics            |
+|  97 | Mathematics            |
+|  98 | Mathematics            |
++-----+------------------------+
+51 rows in set (0.00 sec)
