134d234cfc4d1428c50f1ca07c81539b1b337565
Jesus <chuy8jay@gmail.com>
2016-03-02 11:45:26 -0700

    Fixed 11, 12 and 3

--- Problem2/q11.sql
+++ Problem2/q11.sql
@@ -1,6 +1,11 @@
-SELECT maj.sid, maj.dname
-FROM major maj,
-      (SELECT E.sid
-       FROM enroll E
-       WHERE E.dname != 'Computer Sciences') E
-WHERE maj.sid = E.sid  AND maj.dname != 'Computer Sciences';
+SELECT dept.dname, dept.numphds
+FROM dept
+WHERE dept.dname NOT IN
+      (SELECT maj.dname
+      FROM major maj, 
+      	   (SELECT S.sid csid
+	   FROM enroll E, student S, course C
+	   WHERE E.sid = S.sid
+	   AND E.cno = C.cno
+	   AND E.dname = 'Computer Sciences') cs
+	   WHERE maj.sid = cs.csid);
